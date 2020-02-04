fe604f47b70663527a854e6bcad69447e43c5bb5
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-02-04 16:53:49 -0300

    login component created

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='login'>
+    <h1>Login</h1>
+    <input type='text' v-model='login'>
+    <input type='text' v-model='password'>
+
+    <button v-on:click='getLogin(login,password)'>Login</button>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  },
+  methods: {
+    getLogin (login, password) {
+      console.log(login)
+      console.log(password)
+    }
+  }
+}
+</script>

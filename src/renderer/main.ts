56e8bb7d56854b187caa52b011354ee2dc343f82
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-04-04 23:08:40 -0300

    constants.js added

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import router from '../renderer/router/index'
 import App from './App.vue'
+import { MEDIA_TYPES } from './util/constants'
 
 const app = createApp(App);
 app.use(router);
+
+app.config.globalProperties.$constants = { MEDIA_TYPES };
 
 app.mount('#app');

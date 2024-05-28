26e53d5372d1c4428a3f06becf177308e9671c3d
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-05-28 02:58:45 -0300

    API URL changed to https

--- src/util/tmdb.js
+++ src/util/tmdb.js
@@ -11,8 +11,8 @@ export const theMovieDb = {}
 
 theMovieDb.common = {
   api_key: '<ADD TMDB API KEY HERE>',
-  base_uri: 'http://api.themoviedb.org/3/',
-  images_uri: 'http://image.tmdb.org/t/p/',
+  base_uri: 'https://api.themoviedb.org/3/',
+  images_uri: 'https://image.tmdb.org/t/p/',
   timeout: 5000,
   language: 'en',
   generateQuery: function (options) {
